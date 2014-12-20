7fdb4c6aa1b8af10ad5f66e6e4671c14bcc1f749
Luis Gonzalez <lgonzalez@rvspain.com>
2014-12-20 11:11:42 +0100

    Working steps

--- as10kboy.ino
+++ as10kboy.ino
@@ -6,6 +6,8 @@
  pin 10 is connected to LOAD 
  We have only a single MAX72XX.
  */
+
+int ledrowmap[4][2] = {{7,6},{5,4},{3,2},{1,0}};
  
 LedControl lc=LedControl(12,11,10,1);
 
@@ -18,7 +20,7 @@ LedControl lc=LedControl(12,11,10,1);
 //const int buttonPin = 36;     // left
 
 //red
-const int buttonPin = 26;     // right
+//const int buttonPin = 26;     // right
 //const int buttonPin = 28;     // left
 
 /* we always wait a bit between updates of the display */
@@ -27,6 +29,7 @@ unsigned long delaytime=0;
 int interval = 100;
 unsigned long time;
 unsigned long lasttime;
+int currentstep = -1;
 
 void setup() {
   lc.shutdown(0,false);
@@ -34,32 +37,60 @@ void setup() {
   lc.clearDisplay(0);
   lasttime = millis();
   lc.setLed(0,0,0,false);
+  
+  Serial.begin(9600);
 }
 
-
-
-void loop() { 
+void loop() {
   //single();
   //r[random(0,7)][random(0,7)] = !r[random(0,7)][random(0,7)];
   //lc.clearDisplay(0);
   //lc.setColumn(0,2,B10110000);
   //lc.setChar(0,3,'a',false);
+
+  interval = analogRead(0);
   
   time = millis();
 
   if(time - lasttime > interval){
+    currentstep++;  
+    if(currentstep>15){
+      currentstep=0;
+    } 
+
     lasttime = time;
     tick();
   }
 
 }
 
-
 void tick(){
-  interval = analogRead(0);
   //lc.setLed(0,row,col,r[row][col]);
-  lc.setLed(0,0,0,true);
+  int rowtoprint = currentstep;
+  int coltoprint = 0;
+  if(rowtoprint >7){
+    rowtoprint-=8;
+    coltoprint = 1;
+  }
+  Serial.print("current=");
+  Serial.print(currentstep);
+  Serial.print("row=");
+  Serial.print(rowtoprint);
+  Serial.print(" col=");
+  Serial.println(coltoprint);
+  printStep(0,rowtoprint,coltoprint,true);
+  printStep(1,rowtoprint,coltoprint,true);
+  printStep(2,rowtoprint,coltoprint,true);
+  printStep(3,rowtoprint,coltoprint,true);
   delay(10);
-  lc.setLed(0,0,0,false);
-
+  printStep(0,rowtoprint,coltoprint,false);
+  printStep(1,rowtoprint,coltoprint,false);
+  printStep(2,rowtoprint,coltoprint,false);
+  printStep(3,rowtoprint,coltoprint,false);
 }
+
+
+void printStep(int sequence, int rowtoprint, int coltoprint, boolean state){
+  lc.setLed(0,rowtoprint,ledrowmap[sequence][coltoprint],state);
+}
+
